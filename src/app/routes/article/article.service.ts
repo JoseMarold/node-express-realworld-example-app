50856c71a68cfee21bdf5627de47c1e734e6323a
JoseMarold <jmmr114@gmail.com>
2026-09-05 20:46:07 -0600

    sql innjection

--- src/app/routes/article/article.service.ts
+++ src/app/routes/article/article.service.ts
@@ -239,38 +239,17 @@ export const createArticle = async (article: any, id: number) => {
 };
 
 export const getArticle = async (slug: string, id?: number) => {
-  const article = await prisma.article.findUnique({
-    where: {
-      slug,
-    },
-    include: {
-      tagList: {
-        select: {
-          name: true,
-        },
-      },
-      author: {
-        select: {
-          username: true,
-          bio: true,
-          image: true,
-          followedBy: true,
-        },
-      },
-      favoritedBy: true,
-      _count: {
-        select: {
-          favoritedBy: true,
-        },
-      },
-    },
-  });
+  const query = `SELECT * FROM "Article" WHERE slug = '${slug}' LIMIT 1`;
+  
+  const articles = await prisma.$queryRawUnsafe(query);
+
+  const article = articles[0] || null;
 
   if (!article) {
     throw new HttpException(404, { errors: { article: ['not found'] } });
   }
 
-  return articleMapper(article, id);
+  return article;
 };
 
 const disconnectArticlesTags = async (slug: string) => {
