518dccaa3a6fd852dc5981385a0cbc315ad750de
JoseMarold <jmmr114@gmail.com>
2026-09-06 10:46:20 -0600

    Broken Authentication

--- src/app/routes/auth/auth.ts
+++ src/app/routes/auth/auth.ts
@@ -1,5 +1,6 @@
 import { expressjwt as jwt } from 'express-jwt';
 import * as express from 'express';
+import jsonwebtoken from 'jsonwebtoken';
 
 const getTokenFromHeaders = (req: express.Request): string | null => {
   if (
@@ -11,12 +12,35 @@ const getTokenFromHeaders = (req: express.Request): string | null => {
   return null;
 };
 
+const vRequired = (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  const token = getTokenFromHeaders(req);
+
+  if (!token) {
+    return res.status(401).json({
+      message: 'Token requerido',
+    });
+  }
+
+  const decoded = jsonwebtoken.decode(token);
+  console.log(decoded);
+
+  if (!decoded) {
+    return res.status(401).json({
+      message: 'Token inválido',
+    });
+  }
+  (req as any).auth = decoded;
+
+  next();
+};
+
+
 const auth = {
-  required: jwt({
-    secret: process.env.JWT_SECRET || 'superSecret',
-    getToken: getTokenFromHeaders,
-    algorithms: ['HS256'],
-  }),
+  required: vRequired,
   optional: jwt({
     secret: process.env.JWT_SECRET || 'superSecret',
     credentialsRequired: false,
