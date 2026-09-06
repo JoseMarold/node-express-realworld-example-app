3a74de6bc68c5b937fef8da12b62a6ca94a219db
JoseMarold <jmmr114@gmail.com>
2026-09-06 15:11:46 -0600

    fix: remove insecure code detected by SAST

--- src/security-demo.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from 'express';
-
-export const insecureEval = (req: Request, res: Response) => {
-  const input = req.body.expression;
-
-  const result = eval(input);
-
-  res.json({ result });
-};
